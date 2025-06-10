54531dfe9c34ffbd150fa6844f54a8399ee7c63d
Shihui XU <shxutoki@gmail.com>
2025-06-10 22:38:21 +0900

    添加Netlify部署配置

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://yourname.com', // 换成你的网站URL
   // 如果你需要部署到子目录，可以使用base选项
   // base: '/my-website',
+  output: 'static',
+  adapter: netlify()
 });
